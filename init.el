53a4ecded2db243531b2860dd35b794107357601
Kristian Nygaard Jensen <freeduck@member.fsf.org>
2019-06-17 13:06:59 +0200

    Add dockerfile-mode

--- init.el
+++ init.el
@@ -215,6 +215,9 @@
   :commands (org-babel-execute:racket
              org-babel-expand-body:racket))
 
+(use-package dockerfile-mode
+  :straight t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -222,7 +225,8 @@
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
    (quote
-    ((org-confirm-babel-evaluate)
+    ((dockerfile-image-name . "terratest")
+     (org-confirm-babel-evaluate)
      (org-use-property-inheritance . t)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
